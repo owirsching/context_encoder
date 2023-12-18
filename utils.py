d4849326fd3f8b82f2dc0e4e1c27d6b8c06dd91a
Olivia Wirsching <77694011+owirsching@users.noreply.github.com>
2023-12-18 09:25:42 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import torch
 from PIL import Image
 from torch.autograd import Variable
-
+import torchvision
+import pyperlin
+import numpy as np
 
 def load_image(filename, size=None, scale=None):
     img = Image.open(filename)
@@ -42,3 +44,43 @@ def normalize_batch(batch):
     # batch /= Variable(std)
     batch = torch.div(batch,Variable(std))
     return batch
+
+
+def generate_masks(num_masks, shape_mask):
+    # num_masks = opt.niter # Num of different masks
+    persistance = .4 # Controls the smoothness of the stains' boundaries. Should be float > 0. In practice, < 1
+    threshold = .8 # More or less controls the area of the stains 
+
+    # Mask generation
+    output_size = (num_masks, shape_mask[0], shape_mask[1])
+    gen = torch.Generator()
+
+    octaves = 5 # Controls level of detail. Should be integer 1-9, depending on the mask shape
+    resolutions = [(2 ** i, 2 ** i) for i in range(1, octaves + 1)]
+    factors = [persistance ** i for i in range(octaves)]
+    fp = pyperlin.FractalPerlin2D(output_size, resolutions, factors, generator=gen)
+    noise = fp().numpy()
+
+    masks = []
+    for i, data in enumerate(noise): 
+        generation = data
+        generation = generation - np.min(generation)
+        generation = generation/np.max(generation)
+        generation_t = (generation > .8).astype(np.uint8)
+        mask = generation_t
+        masks.append(mask)
+    
+    return masks
+
+
+# STYLE 
+# The code below could be used to extract style layers from an images
+
+# def extract_features(X, style_layers):
+#     contents = []
+#     styles = []
+#     for i in range(len(net)):
+#         X = net[i](X)
+#         if i in style_layers:
+#             styles.append(X)
+#     return styles
